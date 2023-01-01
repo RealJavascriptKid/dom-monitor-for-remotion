6ec019c83bd229e24416bc9b305402d9c50edb57
RealJavascriptKid <saad.nisar@gmail.com>
2022-12-31 21:33:32 -0500

    compacted computed styles with multiple classes

--- libs/readDomSnaphot.js
+++ libs/readDomSnaphot.js
@@ -1,8 +1,281 @@
+
+let cssPropsHash = {
+    "align-content":{className:'alg'},
+    "align-items":{className:'alg'},
+    "align-self":{className:'alg'},
+    "alignment-baseline":{className:'alg'},
+    "all":{className:'gen'},
+    "animation":{className:'at'},
+    "animation-delay":{className:'at'},
+    "animation-direction":{className:'at'},
+    "animation-duration":{className:'at'},
+    "animation-fill-mode":{className:'at'},
+    "animation-iteration-count":{className:'at'},
+    "animation-name":{className:'at'},
+    "animation-play-state":{className:'at'},
+    "animation-timing-function":{className:'at'},
+    "backface-visibility":{className:'disp'},
+    "background":{className:'bg'},
+    "background-attachment":{className:'bg'},
+    "background-clip":{className:'bg'},
+    "background-color":{className:'bg'},
+    "background-image":{className:'bg'},
+    "background-origin":{className:'bg'},
+    "background-position":{className:'bg'},
+    "background-repeat":{className:'bg'},
+    "background-size":{className:'bg'},
+    "background-blend-mode":{className:'bg'},
+    "baseline-shift":{className:'gen'},
+    "border":{className:'bor'},
+    "border-color":{className:'bor'},
+    "border-style":{className:'bor'},
+    "border-width":{className:'bor'},
+    "border-bottom":{className:'bor'},
+    "border-bottom-color":{className:'bor'},
+    "border-bottom-style":{className:'bor'},
+    "border-bottom-width":{className:'bor'},
+    "border-left":{className:'bor'},
+    "border-left-color":{className:'bor'},
+    "border-left-style":{className:'bor'},
+    "border-left-width":{className:'bor'},
+    "border-right":{className:'bor'},
+    "border-right-color":{className:'bor'},
+    "border-right-style":{className:'bor'},
+    "border-right-width":{className:'bor'},
+    "border-top":{className:'bor'},
+    "border-top-color":{className:'bor'},
+    "border-top-style":{className:'bor'},
+    "border-top-width":{className:'bor'},
+    "border-collapse":{className:'bor'},
+    "border-image":{className:'bor'},
+    "border-image-outset":{className:'bor'},
+    "border-image-repeat":{className:'bor'},
+    "border-image-slice":{className:'bor'},
+    "border-image-source":{className:'bor'},
+    "border-image-width":{className:'bor'},
+    "border-radius":{className:'bor'},
+    "border-bottom-left-radius":{className:'bor'},
+    "border-bottom-right-radius":{className:'bor'},
+    "border-top-left-radius":{className:'bor'},
+    "border-top-right-radius":{className:'bor'},
+    "border-spacing":{className:'bor'},
+    "bottom":{className:'pos'},
+    "box-shadow":{className:'gen'},
+    "box-sizing":{className:'gen'},
+    "break-after":{className:'gen'},
+    "break-before":{className:'gen'},
+    "break-inside":{className:'gen'},
+    "buffered-rendering":{className:'gen'},
+    "caption-side":{className:'gen'},
+    "clear":{className:'pos'},
+    "clip":{className:'gen'},
+    "clip-path":{className:'gen'},
+    "clip-rule":{className:'gen'},
+    "color":{className:'fc'},
+    "color-interpolation":{className:'fc'},
+    "color-interpolation-filters":{className:'fc'},
+    "color-rendering":{className:'fc'},
+    "column-fill":{className:'disp'},
+    "column-gap":{className:'disp'},
+    "column-rule":{className:'disp'},
+    "column-rule-color":{className:'disp'},
+    "column-rule-style":{className:'disp'},
+    "column-rule-width":{className:'disp'},
+    "column-span":{className:'disp'},
+    "columns":{className:'disp'},
+    "column-count":{className:'disp'},
+    "column-width":{className:'disp'},
+    "contain":{className:'gen'},
+    "content":{className:'gen'},
+    "counter-increment":{className:'gen'},
+    "counter-reset":{className:'gen'},
+    "counter-set":{className:'gen'},
+    "cursor":{className:'gen'},
+    "direction":{className:'disp'},
+    "display":{className:'disp'},
+    "dominant-baseline":{className:'gen'},
+    "empty-cells":{className:'gen'},
+    "fill":{className:'gen'},
+    "fill-opacity":{className:'gen'},
+    "fill-rule":{className:'gen'},
+    "filter":{className:'gen'},
+    "float":{className:'gen'},
+    "flex":{className:'disp'},
+    "flex-basis":{className:'disp'},
+    "flex-grow":{className:'disp'},
+    "flex-shrink":{className:'disp'},
+    "flex-flow":{className:'disp'},
+    "flex-direction":{className:'disp'},
+    "flex-wrap":{className:'disp'},
+    "flood-color":{className:'gen'},
+    "flood-opacity":{className:'gen'},
+    "font":{className:'fc'},
+    "font-family":{className:'fc'},
+    "font-size":{className:'fc'},
+    "font-stretch":{className:'fc'},
+    "font-style":{className:'fc'},
+    "font-weight":{className:'fc'},
+    "font-feature-settings":{className:'fc'},
+    "font-kerning":{className:'fc'},
+    "font-variant":{className:'fc'},
+    "font-variant-caps":{className:'fc'},
+    "font-variant-east-asian":{className:'fc'},
+    "font-variant-ligatures":{className:'fc'},
+    "font-variant-numeric":{className:'fc'},
+    "grid":{className:'disp'},
+    "grid-auto-flow":{className:'disp'},
+    "grid-auto-columns":{className:'disp'},
+    "grid-auto-rows":{className:'disp'},
+    "grid-template":{className:'disp'},
+    "grid-template-areas":{className:'disp'},
+    "grid-template-columns":{className:'disp'},
+    "grid-template-rows":{className:'disp'},
+    "grid-area":{className:'disp'},
+    "grid-column":{className:'disp'},
+    "grid-column-start":{className:'disp'},
+    "grid-column-end":{className:'disp'},
+    "grid-row":{className:'disp'},
+    "grid-row-start":{className:'disp'},
+    "grid-row-end":{className:'disp'},
+    "height":{className:'wh'},
+    "hyphens":{className:'gen'},
+    "image-orientation":{className:'gen'},
+    "image-rendering":{className:'gen'},
+    "isolation":{className:'gen'},
+    "justify-content":{className:'alg'},
+    "justify-items":{className:'alg'},
+    "justify-self":{className:'alg'},
+    "left":{className:'pos'},
+    "letter-spacing":{className:'txt'},
+    "lighting-color":{className:'gen'},
+    "line-break":{className:'txt'},
+    "line-height":{className:'txt'},
+    "list-style":{className:'gen'},
+    "list-style-image":{className:'gen'},
+    "list-style-position":{className:'gen'},
+    "list-style-type":{className:'gen'},
+    "margin":{className:'mp'},
+    "margin-bottom":{className:'mp'},
+    "margin-left":{className:'mp'},
+    "margin-right":{className:'mp'},
+    "margin-top":{className:'mp'},
+    "marker":{className:'gen'},
+    "marker-end":{className:'gen'},
+    "marker-mid":{className:'gen'},
+    "marker-start":{className:'gen'},
+    "mask":{className:'gen'},
+    "mask-type":{className:'gen'},
+    "max-height":{className:'wh'},
+    "max-width":{className:'wh'},
+    "min-height":{className:'wh'},
+    "min-width":{className:'wh'},
+    "mix-blend-mode":{className:'gen'},
+    "object-fit":{className:'disp'},
+    "object-position":{className:'disp'},
+    "opacity":{className:'at'},
+    "order":{className:'disp'},
+    "orphans":{className:'gen'},
+    "outline":{className:'disp'},
+    "outline-color":{className:'disp'},
+    "outline-style":{className:'disp'},
+    "outline-width":{className:'disp'},
+    "outline-offset":{className:'disp'},
+    "overflow":{className:'disp'},
+    "overflow-x":{className:'disp'},
+    "overflow-y":{className:'disp'},
+    "overflow-wrap":{className:'disp'},
+    "padding":{className:'mp'},
+    "padding-bottom":{className:'mp'},
+    "padding-left":{className:'mp'},
+    "padding-right":{className:'mp'},
+    "padding-top":{className:'mp'},
+    "page":{className:'gen'},
+    "page-break-after":{className:'gen'},
+    "page-break-before":{className:'gen'},
+    "page-break-inside":{className:'gen'},
+    "paint-order":{className:'gen'},
+    "perspective":{className:'gen'},
+    "perspective-origin":{className:'gen'},
+    "pointer-events":{className:'gen'},
+    "position":{className:'pos'},
+    "quotes":{className:'gen'},
+    "resize":{className:'gen'},
+    "right":{className:'pos'},
+    "ruby-position":{className:'gen'},
+    "scroll-behavior":{className:'gen'},
+    "scroll-snap-type":{className:'gen'},
+    "shape-image-threshold":{className:'gen'},
+    "shape-margin":{className:'gen'},
+    "shape-outside":{className:'gen'},
+    "shape-rendering":{className:'gen'},
+    "size":{className:'gen'},
+    "speak":{className:'gen'},
+    "stop-color":{className:'gen'},
+    "stop-opacity":{className:'gen'},
+    "stroke":{className:'gen'},
+    "stroke-dasharray":{className:'gen'},
+    "stroke-dashoffset":{className:'gen'},
+    "stroke-linecap":{className:'gen'},
+    "stroke-linejoin":{className:'gen'},
+    "stroke-miterlimit":{className:'gen'},
+    "stroke-opacity":{className:'gen'},
+    "stroke-width":{className:'gen'},
+    "tab-size":{className:'gen'},
+    "table-layout":{className:'gen'},
+    "text-align":{className:'txt'},
+    "text-align-last":{className:'txt'},
+    "text-anchor":{className:'txt'},
+    "text-combine-upright":{className:'txt'},
+    "text-decoration":{className:'txt'},
+    "text-decoration-color":{className:'txt'},
+    "text-decoration-line":{className:'txt'},
+    "text-decoration-style":{className:'txt'},
+    "text-indent":{className:'txt'},
+    "text-orientation":{className:'txt'},
+    "text-overflow":{className:'txt'},
+    "text-rendering":{className:'txt'},
+    "text-shadow":{className:'txt'},
+    "text-size-adjust":{className:'txt'},
+    "text-transform":{className:'txt'},
+    "text-underline-position":{className:'txt'},
+    "top":{className:'pos'},
+    "touch-action":{className:'gen'},
+    "transform":{className:'at'},
+    "transform-box":{className:'at'},
+    "transform-origin":{className:'at'},
+    "transform-style":{className:'at'},
+    "transition":{className:'at'},
+    "transition-delay":{className:'at'},
+    "transition-duration":{className:'at'},
+    "transition-property":{className:'at'},
+    "transition-timing-function":{className:'at'},
+    "unicode-bidi":{className:'gen'},
+    "vector-effect":{className:'gen'},
+    "vertical-align":{className:'alg'},
+    "visibility":{className:'disp'},
+    "white-space":{className:'gen'},
+    "widows":{className:'gen'},
+    "width":{className:'wh'},
+    "will-change":{className:'gen'},
+    "word-break":{className:'gen'},
+    "word-spacing":{className:'gen'},
+    "word-wrap":{className:'gen'},
+    "writing-mode":{className:'gen'},
+    "z-index":{className:'pos'},
+    }
+
 module.exports = (snapshot,computedCSSProps,overrideSettings = {}) => {
 
     let settings = {
         replaceSources:false,
-        excludeNodeNames:['script','style','link','meta','iframe','noscript'],
+        applyComputedStyles:true,
+        excludeNodeNames:['script'
+                        //    ,'style'
+                        //    ,'link'
+                        //    ,'meta'
+                            ,'iframe'
+                            ,'noscript'
+                        ],
         nodeNamesToExcludeComputedSyles:['html','link','head','title','meta','noscript'],
     }
 
@@ -18,7 +291,7 @@ module.exports = (snapshot,computedCSSProps,overrideSettings = {}) => {
         baseURL = strings[doc.baseURL],
         nodesFlattened = [], //hash of all nodes as flattened
         rootNodes = [],
-        cssRulesHash = {},cssRulesCount = 0;
+        cssRulesHash = {},cssRulesByClassCounters = {};
 
     if(baseURL.endsWith('/'))
         baseURL = baseURL.slice(0, -1);
@@ -26,17 +299,33 @@ module.exports = (snapshot,computedCSSProps,overrideSettings = {}) => {
     const hashCode = s => s.split('').reduce((a,b) => (((a << 5) - a) + b.charCodeAt(0))|0, 0);
 
 
-    let getClassForCSSRules = (cssRules) => {
-        let hash = hashCode(cssRules);
-        if(!cssRulesHash[hash]){
-            let className = `style${++cssRulesCount}`;
-            cssRulesHash[hash] = className;
-            css += `
-            .${className} {
-                ${cssRules};
-            }`
+    let getClassForCSSRules = (cssRulesByClass) => {
+
+        if(!settings.applyComputedStyles)
+            return '';
+
+        let classes = [];
+
+        for(let clsPrefix in cssRulesByClass){
+
+            let cssRules = cssRulesByClass[clsPrefix];
+            let hash = hashCode(cssRules);
+            if(!cssRulesHash[hash]){
+                if(!cssRulesByClassCounters[clsPrefix])
+                    cssRulesByClassCounters[clsPrefix] = 0;
+                
+                let className = `${clsPrefix}-${++cssRulesByClassCounters[clsPrefix]}`;
+                cssRulesHash[hash] = className;
+                css += `
+                .${className} {
+                    ${cssRules};
+                }`
+            }
+
+            classes.push(cssRulesHash[hash]);
         }
-        return cssRulesHash[hash]
+        
+        return classes.join(' ')
     }
         
     let buildAttributes = (arr) => {
@@ -68,17 +357,19 @@ module.exports = (snapshot,computedCSSProps,overrideSettings = {}) => {
         }
 
         //build style attribute using computed style
-        let cssRules = '';
+        let cssRulesByClass = {};
         for(let cssRule in node.styles){
-            if(node.styles[cssRule] != null)
-                cssRules += `${cssRule}:${node.styles[cssRule].replaceAll('"',"'")};`
+            if(node.styles[cssRule] != null){
+                let className = cssPropsHash[cssRule].className
+                if(!cssRulesByClass[className])
+                    cssRulesByClass[className] = '';
+
+                cssRulesByClass[className] += `${cssRule}:${node.styles[cssRule].replaceAll('"',"'")};`
+            }              
         }
 
-        if(cssRules.length){
-            let additionalClasses = (node.attributes['class'] != null)?node.attributes['class']:''
-            att += '\n class="' + getClassForCSSRules(cssRules) + ' ' + additionalClasses   + '"'
-        }
-            
+        let additionalClasses = (node.attributes['class'] != null)?node.attributes['class']:''
+        att += '\n class="' + getClassForCSSRules(cssRulesByClass) + ' ' + additionalClasses   + '"'
 
         return att;
     }
